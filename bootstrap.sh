e1aa6b24d441cd4b43559a7d063c7fda3c9d25d2
Matteo Scotuzzi <matteo.scotuzzi@gmail.com>
2011-05-04 15:33:52 +0200

    better automated prerequisite package installation for bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-sudo apt-get install git
+sudo apt-get -q -y install git
 git clone https://scotu@github.com/scotu/chef-ubuntu-hoth.git /tmp/chef-ubuntu-hoth
-sudo apt-get install ruby1.8 ruby1.8-dev ri1.8 libruby1.8 rubygems1.8 build-essential wget ssl-cert
+sudo apt-get -q -y install ruby1.8 ruby1.8-dev ri1.8 libruby1.8 rubygems1.8 build-essential wget ssl-cert
 sudo gem install chef
 sudo /var/lib/gems/1.8/bin/chef-solo -c /tmp/chef-ubuntu-hoth/solo.rb
