d78dfd1d885cb1b44821d44f73ba4dcd6df5b724
Matteo Scotuzzi <matteo.scotuzzi@gmail.com>
2011-05-04 15:21:45 +0200

    Fixed Chef command in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ sudo apt-get install git
 git clone https://scotu@github.com/scotu/chef-ubuntu-hoth.git /tmp/chef-ubuntu-hoth
 sudo apt-get install ruby1.8 ruby1.8-dev ri1.8 libruby1.8 rubygems1.8 build-essential wget ssl-cert
 sudo gem install chef
-/var/lib/gems/1.8/bin/chef-solo -c /tmp/chef-ubuntu-hoth/solo.rb
+sudo /var/lib/gems/1.8/bin/chef-solo -c /tmp/chef-ubuntu-hoth/solo.rb
